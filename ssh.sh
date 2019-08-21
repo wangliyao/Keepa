ab69828a74b1a5e18de5346fdbce1a0d6121ac3b
wangliyao <755210570@qq.com>
2019-08-21 16:29:28 +0800

    部署

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,24 @@
 branch=$(git symbolic-ref --short -q HEAD)
 echo "分支: ${branch}"
-deploy.sh master
+app= 'master'
+cd /root/workspace/
+if [ ! -d "${app}" ];then
+    echo "克隆项目${app}"
+    git clone -b $app https://github.com/wangliyao/Keepa.git $app
+    cd ${app}
+else
+    cd ${app}
+    echo "拉取项目${app}"
+    git pull origin $app
+fi
+branch=${app//-/}
+echo ${branch}
+web=$(docker-compose ps | grep $branch | awk '{print $1}')
+if [ -z $web ];then
+    echo "构建容器: ${web}"
+    docker-compose up -d
+else
+    echo "重启容器: ${web}"
+    docker-compose restart
+fi
 
